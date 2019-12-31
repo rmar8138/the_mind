38880ab2b30737b141c9dcde10355e7db0b9db2d
rmar8138 <ragan.martinez@live.com>
2020-01-01 00:26:07 +1100

    WHy heroku why

--- server/app.js
+++ server/app.js
@@ -7,12 +7,6 @@ const routes = require("./routes");
 const app = express();
 const db = process.env.MONGODB_URI || "mongodb://localhost/the_mind";
 
-app.use(express.static(path.join(__dirname, "./../build/static")));
-
-app.get("*", (req, res) => {
-  res.sendfile(path(__dirname + "./../build/index.html"));
-});
-
 mongoose.connect(db, {
   useNewUrlParser: true,
   useUnifiedTopology: true
@@ -25,6 +19,12 @@ mongoose.connection.on("error", err => console.log(err));
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+
+app.use(express.static(path.join(__dirname, "./../build/static")));
+
+app.get("*", (req, res) => {
+  res.sendfile(path(__dirname + "./../build/index.html"));
+});
 
 app.use(routes);
 
