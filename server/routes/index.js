cc27edaa2c6a94af8d1e65667b8ec19c81b85859
rmar8138 <ragan.martinez@live.com>
2019-12-31 20:49:02 +1100

    Update cors

--- server/routes/index.js
+++ server/routes/index.js
@@ -3,14 +3,9 @@ const path = require("path");
 const router = express.Router();
 const RoomModel = require("../database/models/RoomModel");
 
-const clientRoute =
-  process.env.NODE_ENV === "production"
-    ? "https://quiet-tor-63256.herokuapp.com/"
-    : "http://localhost:3000";
-
 // enable cors
 router.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", clientRoute); // update to match the domain you will make the request from
+  res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
   res.header(
     "Access-Control-Allow-Headers",
     "Origin, X-Requested-With, Content-Type, Accept"
