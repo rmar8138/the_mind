3257edb1a9062cacf9e35ca86d46bc7e27ee66ab
rmar8138 <ragan.martinez@live.com>
2020-01-01 00:46:00 +1100

    WHy heroku why

--- server/app.js
+++ server/app.js
@@ -20,7 +20,7 @@ mongoose.connection.on("error", err => console.log(err));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-app.use(express.static(path.join(__dirname, "./../build/static")));
+app.use(express.static(path.join(__dirname, "./../build/")));
 
 app.use(routes);
 
